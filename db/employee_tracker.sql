a976f4724bde700e0bafbeea59479d016f9138ae
Gerry Paul <gpaul12@nc.rr.com>
2023-04-17 12:47:19 -0400

    create department table

--- db/employee_tracker.sql
+++ db/employee_tracker.sql
@@ -2,3 +2,8 @@ DROP DATABASE IF EXISTS employee_tracker;
 CREATE DATABASE employee_tracker;
 
 USE employee_tracker;
+
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  d_name VARCHAR(30)
+);
