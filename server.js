8665b6439bde85322de3974dc9e3d1921edb80ad
Gerry Paul <gpaul12@nc.rr.com>
2023-04-17 12:24:35 -0400

    logo requirements

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const fs = require("fs");
 const cTable = require("console.table");
 var mysql = require("mysql");
 const util = require("util");
+
+const logo = require("asciiart-logo");
+const config = require("./package.json");
+console.log(logo(config).render());
