e5f5757f744793017034bf89e91caeaaeab28777
Gerry Paul <gpaul12@nc.rr.com>
2023-04-17 12:27:14 -0400

    mysql connection

--- server.js
+++ server.js
@@ -7,3 +7,11 @@ const util = require("util");
 const logo = require("asciiart-logo");
 const config = require("./package.json");
 console.log(logo(config).render());
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "12345",
+  database: "employee_tracker",
+});
