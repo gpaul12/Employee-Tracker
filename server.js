3bff4d51915d94c49c29f15318fbe2eb582c3b14
Gerry Paul <gpaul12@nc.rr.com>
2023-04-18 13:47:04 -0400

    require mysql2 for auth err

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const cTable = require("console.table");
-var mysql = require("mysql");
+const mysql = require("mysql2");
 const util = require("util");
 
 const logo = require("asciiart-logo");
